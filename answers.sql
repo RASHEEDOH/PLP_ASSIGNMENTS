966589d2c657e05e808cef9a7c33893e7ebbd131
Rashid Mugi <130837638+RASHEEDOH@users.noreply.github.com>
2025-03-21 12:39:04 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,9 @@
+**CREATING DATABASE SALESDB**
+CREATE DATABASE salesDB;
+
+ **DROPPING DATABASE DEMO**
+In order to create the databasecalled Demo, we have to create IT first as the database does not exist
+CREATE DATABASE demoDB;
+
+Now we can drop the database
+DROP DATABASE demodb;
